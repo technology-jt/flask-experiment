68e640d48766892628b90e3c8a28f47d09b89690
jthomas16882 <technology@justintheodore.me>
2022-03-27 16:31:20 -0400

    end of episode 3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, render_template, url_for
+from flask import Flask, escape, request, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
