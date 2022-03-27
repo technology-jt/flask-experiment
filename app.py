c9bcc7a324813d89d5bd184c01c2367f0528a1c6
jthomas16882 <technology@justintheodore.me>
2022-03-27 13:03:48 -0400

    episode 3 - forms and user input

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ posts = [
 ]
 
 
-@app.route('/')
+@app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
@@ -32,10 +32,26 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
